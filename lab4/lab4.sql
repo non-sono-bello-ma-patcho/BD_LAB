9489ff73deefb4b661dba15545e35e9df1b75bfd
rollingflamingo <andrea.storace.uni@gmail.com>
2018-04-24 18:32:13 +0200

    Removed unuseful comment from lab3 query. Working on lab4 query, not sure about 5th exercise. Still have to check from 6th below.

--- lab4/lab4.sql
+++ lab4/lab4.sql
@@ -1,4 +1,4 @@
-﻿set search_path to unicorsi;
+set search_path to unicorsi;
 --FUNZIONI DI GRUPPO-------
 
 ----esercizio1----
@@ -14,19 +14,33 @@ where corsidilaurea.denominazione='Informatica';
 
 ----esercizio3----
 select max(voto),corsidilaurea.denominazione
+/* 	right outer join è necessario perché il join naturale escluderebbe
+	tutte le tuple che non hanno voto, dal momento che noi vogliamo che
+	vengano mostrati i voti per ogni corso di laurea, dobbiamo specificare
+	sul secondo join che siano mostrate tutte le tuple per ogni corso diverso
+*/
 from esami join corsi on esami.corso=corsi.id
-	join corsidilaurea on corsi.corsodilaurea=corsidilaurea.id
+	right outer join corsidilaurea on corsi.corsodilaurea=corsidilaurea.id
+group by corsidilaurea.denominazione -- la clausola group by è sempre necessaria nel caso di funzioni aggregate come max;
+order by max(voto) desc;
+
+-- avremmo potuto anche scrivere:
+select max(voto), corsidilaurea.denominazione
+from corsidilaurea left outer join corsi on corsi.corsodilaurea = corsidilaurea.id
+left outer join esami on corsi.id = esami.corso
 group by corsidilaurea.denominazione
 order by max(voto) desc;
+-- notare che in questo caso l'outer left join va propagato...
 
 
 ----esercizio4---
-select nome, cognome/*,corsi.denominazione,attivato,attivazione,*/,count(*)
-from professori 
-join corsi on corsi.professore=professori.id
-join corsidilaurea on corsi.corsodilaurea=corsidilaurea.id
+select nome, cognome, count(*)
+from professori
+left outer join corsi on corsi.professore=professori.id
+left outer join corsidilaurea on corsi.corsodilaurea=corsidilaurea.id
 group by nome,cognome having count(*)>2 --having filtra il risultato di group by ... una specie di where
 order by count(*)  desc;
+
 
 ---esercizio5---
 select corsi.denominazione,count(*)
@@ -41,15 +55,13 @@ select esami.studente,corsi.denominazione,data
 from esami
 full outer join corsi on esami.corso=corsi.id
 join corsidilaurea on corsi.corsodilaurea=corsidilaurea.id
-where corsidilaurea.denominazione='Informatica'
-
-;
+where corsidilaurea.denominazione='Informatica';
 
 --esercizio6---
 select professori.cognome,professori.nome,count(matricola)
 from professori
 join studenti on studenti.relatore=professori.id
-group by professori.cognome,professori.nome 
+group by professori.cognome,professori.nome
 order by professori.cognome,professori.nome asc;
 
 
@@ -98,24 +110,3 @@ select matricola
 from studenti
 join corsidilaurea on studenti.corsodilaurea=corsidilaurea.id as f
 where corsidilaurea.denominazione='Informatica';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
