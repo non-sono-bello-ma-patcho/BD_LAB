33d92f0ebdfeb53ff2591b1f7a096125a6d25d43
Strafo <aspem2015@gmail.com>
2018-04-24 12:17:48 +0200

    esercizio4

---
+++ lab4/sqles4.sql
@@ -0,0 +1,39 @@
+﻿SET search_path TO 'unicorsi';
+
+
+SELECT max(stipendio),min(stipendio),avg(stipendio)
+FROM professori;
+
+
+SELECT  max(esami.voto) ,avg(voto),min(voto),corsi.denominazione,corsidilaurea.denominazione
+FROM esami JOIN corsi ON esami.corso=corsi.id
+	JOIN corsidilaurea ON corsi.corsodilaurea=corsidilaurea.id
+WHERE corsidilaurea.denominazione='Informatica'
+GROUP BY corsi.denominazione,corsidilaurea.denominazione;
+
+
+
+
+SELECT  max(esami.voto) ,avg(voto),min(voto),corsi.denominazione,corsidilaurea.denominazione
+FROM esami JOIN corsi ON esami.corso=corsi.id
+	JOIN corsidilaurea ON corsi.corsodilaurea=corsidilaurea.id
+GROUP BY corsi.denominazione,corsidilaurea.denominazione
+ORDER BY corsidilaurea.denominazione ASC;
+
+
+
+---esercizio 4 da completare----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
