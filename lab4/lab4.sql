9484bd58525d55bdaffb165a6ead50212008dead
rollingflamingo <andrea.storace.uni@gmail.com>
2018-05-06 21:52:34 +0200

    Solved 3rd exercise of 5th drill, hope next one will take me less time..

--- lab4/lab4.sql
+++ lab4/lab4.sql
@@ -13,17 +13,17 @@ where corsidilaurea.denominazione='Informatica';
 
 
 ----esercizio3----
-select max(voto),corsidilaurea.denominazione
+select corsidilaurea.facolta, corsidilaurea.denominazione, max(voto)
 /* 	right outer join è necessario perché il join naturale escluderebbe
 	tutte le tuple che non hanno voto, dal momento che noi vogliamo che
 	vengano mostrati i voti per ogni corso di laurea, dobbiamo specificare
 	sul secondo join che siano mostrate tutte le tuple per ogni corso diverso
 */
 from esami join corsi on esami.corso=corsi.id
-	right outer join corsidilaurea on corsi.corsodilaurea=corsidilaurea.id
-group by corsidilaurea.denominazione -- la clausola group by è sempre necessaria nel caso di funzioni aggregate come max;
+						join corsidilaurea on corsi.corsodilaurea=corsidilaurea.id
+group by corsidilaurea.denominazione, corsidilaurea.facolta -- la clausola group by è sempre necessaria nel caso di funzioni aggregate come max;
 order by max(voto) desc;
-
+/*
 -- avremmo potuto anche scrivere:
 select max(voto), corsidilaurea.denominazione
 from corsidilaurea left outer join corsi on corsi.corsodilaurea = corsidilaurea.id
@@ -31,7 +31,7 @@ left outer join esami on corsi.id = esami.corso
 group by corsidilaurea.denominazione
 order by max(voto) desc;
 -- notare che in questo caso l'outer left join va propagato...
-
+*/
 
 ----esercizio4---
 select nome, cognome, count(*)
@@ -43,19 +43,19 @@ order by count(*)  desc;
 
 
 ---esercizio5---
-select corsi.denominazione,count(*)
+select corsi.denominazione, count(esami) as "Numero Esami"
+from corsi
+	left outer join esami on esami.corso=corsi.id
+	left outer join corsidilaurea on corsi.corsodilaurea=corsidilaurea.id
+where esami.data > '2012-01-04'
+group by corsi.denominazione having count(*) = 0
+union
+select corsi.denominazione, count(*) as "Numero Esami"
 from esami
-join corsi on esami.corso=corsi.id
-join corsidilaurea on corsi.corsodilaurea=corsidilaurea.id
-where corsidilaurea.denominazione='Informatica'
-group by corsi.denominazione having count(*)<5
-;/*
-union*/
-select esami.studente,corsi.denominazione,data
-from esami
-full outer join corsi on esami.corso=corsi.id
-join corsidilaurea on corsi.corsodilaurea=corsidilaurea.id
-where corsidilaurea.denominazione='Informatica';
+	right outer join corsi on esami.corso=corsi.id
+	left outer join corsidilaurea on corsi.corsodilaurea=corsidilaurea.id
+where corsidilaurea.denominazione='Informatica' and esami.data > '2012-01-04'
+group by corsi.denominazione having count(*)>5;/*  questa parte va bene */
 
 --esercizio6---
 select professori.cognome,professori.nome,count(matricola)
@@ -87,7 +87,7 @@ where corsidilaurea.denominazione='Informatica'
 group by matricola having count(extract( month from esami.data))>=2
 order by matricola;
 
-
+/*
 ----SOTTO INTERROGAZIONI--------
 
 --esercizio1 ----
@@ -108,5 +108,6 @@ group by corsidilaurea.denominazione having count(*)>0;/*>(select count(*)
 
 select matricola
 from studenti
-join corsidilaurea on studenti.corsodilaurea=corsidilaurea.id as f
+join corsidilaurea on studenti.corsodilaurea=corsidilaurea.id as "f"
 where corsidilaurea.denominazione='Informatica';
+*/
--- lab5/lab5.sql
+++ lab5/lab5.sql
@@ -2,24 +2,24 @@
 set search_path to unicorsi;
 
 --1
---sotto-interrogazione correlata 
+--sotto-interrogazione correlata
 SELECT matricola
 	FROM esami
 	JOIN studenti S  on esami.studente=S.matricola
 	JOIN corsi on esami.corso=corsi.id
-	JOIN corsidilaurea ON S.corsodilaurea=corsidilaurea.id 
-	WHERE corsi.denominazione='Basi Di Dati 1' 
-	AND corsidilaurea.denominazione='Informatica' 
-	AND esami.data BETWEEN '2010-06-01' AND '2010-06-30' 
+	JOIN corsidilaurea ON S.corsodilaurea=corsidilaurea.id
+	WHERE corsi.denominazione='Basi Di Dati 1'
+	AND corsidilaurea.denominazione='Informatica'
+	AND esami.data BETWEEN '2010-06-01' AND '2010-06-30'
 
 	AND EXISTS(
-	
+
 	SELECT S.matricola
 	FROM esami
 	JOIN studenti on esami.studente=S.matricola
 	JOIN corsi on esami.corso=corsi.id
-	WHERE corsi.denominazione='Interfacce Grafiche' 
-	AND corsidilaurea.denominazione='Informatica' 
+	WHERE corsi.denominazione='Interfacce Grafiche'
+	AND corsidilaurea.denominazione='Informatica'
 	AND esami.data BETWEEN '2010-06-01' AND '2010-06-30');
 
 --2
@@ -27,29 +27,53 @@ SELECT matricola
 	FROM esami
 	JOIN studenti  S on esami.studente=S.matricola
 	JOIN corsi on esami.corso=corsi.id
-	JOIN corsidilaurea ON S.corsodilaurea=corsidilaurea.id 
-	WHERE corsi.denominazione='Basi Di Dati 1' 
-	AND corsidilaurea.denominazione='Informatica' 
-	AND esami.data BETWEEN '2010-06-01' AND '2010-06-30' 
+	JOIN corsidilaurea ON S.corsodilaurea=corsidilaurea.id
+	WHERE corsi.denominazione='Basi Di Dati 1'
+	AND corsidilaurea.denominazione='Informatica'
+	AND esami.data BETWEEN '2010-06-01' AND '2010-06-30'
 
 	AND NOT EXISTS(
-	
+
 	SELECT S.matricola
 	FROM esami
 	JOIN studenti on esami.studente=S.matricola
 	JOIN corsi on esami.corso=corsi.id
-	WHERE corsi.denominazione='Interfacce Grafiche' 
-	AND corsidilaurea.denominazione='Informatica' 
+	WHERE corsi.denominazione='Interfacce Grafiche'
+	AND corsidilaurea.denominazione='Informatica'
 	AND esami.data BETWEEN '2010-06-01' AND '2010-06-30');
 
 
 --3
 
-/*select matricola
-from studenti S 
-JOIN corsidilaurea ON S.corsodilaurea=corsidilaurea.id 
-group by matricola having AVG (select esami.voto
-	from esami 
-	join studenti on S.matricola = esami.studente)
+-- insieme delle matricole
+select matricola
+from studenti S
+JOIN corsidilaurea ON S.corsodilaurea=corsidilaurea.id
+group by matricola /*having AVG (select esami.voto
+	from esami
+	join studenti on S.matricola = esami.studente)*/;
 
-	;*/
+-- insieme dei voti di una matricola
+select matricola
+from( -- insieme degli studenti di informatica + media voti:
+	select matricola, AVG(esami.voto)as media
+	from studenti S
+	join esami
+		on esami.studente = S.matricola
+	join corsidilaurea
+		on corsidilaurea.id = S.corsodilaurea
+	where corsidilaurea.denominazione = 'Informatica'
+	group by matricola
+)mm
+where mm.media = (select max(media) from(
+		select matricola, AVG(esami.voto)as media
+		from studenti S
+		join esami
+			on esami.studente = S.matricola
+		join corsidilaurea
+			on corsidilaurea.id = S.corsodilaurea
+		where corsidilaurea.denominazione = 'Informatica'
+		group by matricola
+
+) asp
+;
